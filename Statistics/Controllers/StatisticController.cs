94c739da716af8dea33aa53e47a4acd2a95d5cee
Anders Fibiger <anders.afj@gmail.com>
2020-08-06 17:16:44 +0200

    initial commit

---
+++ Statistics/Controllers/StatisticController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatisticController
+    {
+
+
+        [HttpGet]
+        public Task GetStatics()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
